a680f38e395e4ee121a866911c08a5b4265b7d6e
Luka M <164436043+diamond-ore@users.noreply.github.com>
2024-04-23 12:57:40 -0400

    Checkerboard fix
    Swapped odd and even, I had dark mode on so I thought I had the black in the top corner but it was technically wrong

--- warmups/block3/for_loops/checker.py
+++ warmups/block3/for_loops/checker.py
@@ -1,8 +1,8 @@
 def checker(n:int) -> str:
     out = ""
 
-    odd = f"{chr(9608)}{chr(9608)}  " * int(n / 2 + n / 2 - n // 2)
-    even = f"  {chr(9608)}{chr(9608)}" * (n // 2)
+    even = f"{chr(9608)}{chr(9608)}  " * int(n / 2 + n / 2 - n // 2)
+    odd = f"  {chr(9608)}{chr(9608)}" * (n // 2)
 
     for i in range(n):
         if i > 0:
