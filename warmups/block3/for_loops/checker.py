7d602dbe7b8b5c48e134a34507a65ee6bf063919
Luka M <164436043+diamond-ore@users.noreply.github.com>
2024-04-24 12:30:45 -0400

    Checker fix
    Ok now this is the last one

--- warmups/block3/for_loops/checker.py
+++ warmups/block3/for_loops/checker.py
@@ -1,8 +1,13 @@
 def checker(n:int) -> str:
     out = ""
 
-    even = f"{chr(9608)}{chr(9608)}  " * (n - n // 2)
+    even = f"{chr(9608)}{chr(9608)}  " * (n - n // 2 - 1) + f"{chr(9608)}{chr(9608)}"
     odd = f"  {chr(9608)}{chr(9608)}" * (n // 2)
+
+    if n % 2 == 0:
+        even += "  "
+    else:
+        odd += "  "
 
     for i in range(n):
         if i > 0:
